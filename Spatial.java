33cddbf8880e873ce7a212a3f91bfd7a77994db8
Kadekk3939 <48683645+Kadekk3939@users.noreply.github.com>
2020-01-26 15:03:04 +0100

    Update Spatial.java

--- Spatial.java
+++ Spatial.java
@@ -4,11 +4,11 @@ import java.awt.geom.Rectangle2D;
 
 public final class Spatial {
   private Spatial() {
-
+    //empty - 'math' class inspired
   }
   
   //rect&rect
-  public static Result relate(Rectangle2D.Double Rect1, Rectangle2D.Double Rect2) {
+  public static Result relate(Rectangle2D Rect1, Rectangle2D Rect2) {
     Result result = Result.NONE;
 
 
@@ -16,26 +16,42 @@ public final class Spatial {
   }
 
   //rect&line
+  public static Result relate(Rectangle2D Rect, Line2D Line) {
+    Result result = Result.NONE;
 
+
+    return result;
+  }
 
 
   //rect&point
+  public static Result relate(Rectangle2D Rect, Point2D Point) {
+    Result result = Result.NONE;
 
+
+    return result;
+  }
 
 
   //line&line
+  public static Result relate(Line2D Line1, Line2D Line2) {
+    Result result = Result.NONE;
+    if (Line1.intersectsLine(Line2)) return result;
+    else return result;
 
+  }
 
 
   //line&point
-  public static Result relate(Line2D.Double Line, Point2D.Double Point) {
+  public static Result relate(Line2D Line, Point2D Point) {
+    Result result = Result.NONE;
     if (Line.contains(Point)) result = COVERS; //there isn't ANYINTERSECT nor CONTAINS relation
     else result = DISJOINT;
     return result;
   }
 
   //point&point
-  public static Result relate(Point2D.Double Point1, Point2D.Double Point2) {
+  public static Result relate(Point2D Point1, Point2D Point2) {
     Result result = Result.NONE;
     if (Point1.equals(Point2)) {
       result = EQUAL; //there isn't ANYINTERSECT, CONTAINS nor COVERS relation
@@ -43,7 +59,4 @@ public final class Spatial {
 
     return result;
   }
-
-
-  
 }
